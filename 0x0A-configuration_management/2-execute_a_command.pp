cd1ed3ebeca2107ffed16ed6e3c16e7e5430f650
Gmwongela <gvidla73@gmail.com>
2023-09-23 16:16:22 +0000

    Configuration management

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# Puppet manifest to kill a process named "killmenow" using pkill
+
+exec { 'killmenow':
+  command     => 'pkill -f "killmenow"',
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
+  onlyif      => 'pgrep -f "killmenow"',
+}
